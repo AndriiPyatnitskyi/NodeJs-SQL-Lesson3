ab00fe885a97abb4c35334a3793484209526fcb7
AndriiPyatnitskyi <pyatnitskiy.a@gmail.com>
2021-11-14 00:14:38 +0200

    Fix auth

--- src/routes/tokens.ts
+++ src/routes/tokens.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import controller from '../controller/tokens';
+import {Role} from "../model/account";
+
+const permit = require("../middleware/auth");
 
 const tokenRouter = express.Router();
 
-tokenRouter.get('/api/tokens', controller.getTokens);
+tokenRouter.get('/api/tokens', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.getTokens);
 
 export default tokenRouter;
