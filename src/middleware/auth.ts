9626d8a57e6aaf937effb75dbcfb71990ffe87e2
AndriiPyatnitskyi <pyatnitskiy.a@gmail.com>
2021-11-14 03:51:36 +0200

    Fix auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -11,25 +11,25 @@ const permit = (permittedRoles: [Role]) => {
 
         if (permittedRoles.includes(Role.ANONYMOUS)) {
             next();
-        }
-
-        const token = req.body?.token || req.query.token || req.headers["x-access-token"];
-
-        if (!token) {
-            return res.status(403).send("A token is required for authentication");
-        }
-
-        let user: Account;
-        try {
-            user = jwt.verify(token, secretKey);
-        } catch (err) {
-            return res.status(401).send("Invalid Token");
-        }
-
-        if (permittedRoles.includes(user.role)) {
-            next(); // role is allowed, so continue on the next middleware
         } else {
-            res.status(403).json({message: "Forbidden. Role isn't allowed."}); // user is forbidden
+            const token = req.body?.token || req.query.token || req.headers["x-access-token"];
+
+            if (!token) {
+                return res.status(403).send("A token is required for authentication");
+            }
+
+            let user: Account;
+            try {
+                user = jwt.verify(token, secretKey);
+            } catch (err) {
+                return res.status(401).send("Invalid Token");
+            }
+
+            if (permittedRoles.includes(user.role)) {
+                next(); // role is allowed, so continue on the next middleware
+            } else {
+                res.status(403).json({message: "Forbidden. Role isn't allowed."}); // user is forbidden
+            }
         }
     }
 }
